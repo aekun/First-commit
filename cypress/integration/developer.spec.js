ca87d9c266080710aaf473139217fceeed9963f3
aekun <michael_dada2003@yahoo.com>
2020-11-21 18:59:37 +0000

    First commit

--- cypress/integration/developer.spec.js
+++ cypress/integration/developer.spec.js
@@ -1,15 +1,23 @@
 describe('developerj',function(){
 
   it('Visit the URL',function(){
-      cy.visit('http://a.testaddressbook.com/');
+      cy.visit('http://automationpractice.com/index.php?controller=order');
   });
 
   it('Click on calculator',function () {
-    cy.get('#sign-in').click();
-    cy.get('#session_email').type('michael');
-    cy.get('#session_password').type('forever');
-    //cy.get('#Sign-up').click();
-    //cy.get('#user_email').type('Test');
+    cy.get('#searchbox').type('women dress');
+    cy.get('.button-search').click();
+    cy.get('.cat-title').click({force: true});
+    cy.get('#contact-link').click();
+    cy.get('#uniform-id_contact').click();
+    cy.get('select').select('Customer service');
+    cy.get('select').select('Webmaster');
+    cy.get('#email').type('shawn');
+    cy.get('#email').type('shawnesco@hotmail.co.uk');
+    cy.get('#id_order').type('bfbsvbj');
+    cy.get('#message').type('gfrhjfgfruygbfgukzg');
+    cy.get('.submit').click();
+    //cy.get('#data-validate=isEmail').type('shawnesco@hotmail.co.uk');
 
 
 
